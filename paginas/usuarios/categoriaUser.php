6c6a7df69a690185a4469ed907851e09db512471
DannyT8 <danielnino.tohou@gmail.com>
2023-04-03 22:08:32 -0500

    alerta de usuario inhabilitado

--- paginas/usuarios/categoriaUser.php
+++ paginas/usuarios/categoriaUser.php
@@ -3,14 +3,7 @@
     if(!isset($_SESSION["Usuario"])){
         header('Location: ../index.php');
     } 
-    if(isset($_SESSION["Usuario"]) && ($_SESSION["estado"] == "inactivo")){
-        echo "<script>
-        $(document).ready(function(){
-            $('#myModal').modal('show');
-            $('.modal-body').html('Aquí se mostrará el mensaje de validación.');
-        });
-      </script>";
-    }
+    
 
     $_SESSION["Usuario"];
 ?>
@@ -21,22 +14,16 @@
     <?php
         include "../../modules/menu/menu_usuario.php"
     ?>
-    <div id="myModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h4 class="modal-title">Mensaje de validación</h4>
-      </div>
-      <div class="modal-body">
-        <p>Aquí se mostrará el mensaje de validación.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-      </div>
-    </div>
-  </div>
-</div>
-
+<?php
+    if(isset($_SESSION["Usuario"]) && ($_SESSION["estado"] == "inactivo")){
+        echo "<div class='alert alert-danger alert-dismissible fade show'>
+                 <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
+                 <strong>Ojo!</strong> Este usuario esta inhabilitado tiene un maximo de 30 días o será eliminado 
+                 <br>
+                 <strong>Dias restantes : </strong> 14
+             </div>"; 
+     }
+?>
    <div class="container">
         <div class="row justify-content-center">
             <div class="col-4 p-4">
