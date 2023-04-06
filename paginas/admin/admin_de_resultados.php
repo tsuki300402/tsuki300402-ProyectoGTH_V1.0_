3a996d1829828f71f13e2306b627f8ca373a5986
alfonso300402 <rooseveltrodriguez3004@gmail.com>
2023-04-06 18:11:08 -0500

    agregado plantillas de los correos

--- paginas/admin/admin_de_resultados.php
+++ paginas/admin/admin_de_resultados.php
@@ -8,7 +8,7 @@
 <?php
         include "../../modules/menu/menu_admin.php"
     ?>
-        <div class="container">
+        <div class="container text-center">
           <div class="row">
             <div class="col-md-12">
               <h2>Lista de los aspirantes que realizaron las pruebas psicotecnicas</h2>
@@ -25,22 +25,26 @@
                               <th>Nivel de prueba</th>   
                           </tr>
                       </thead>
-                      <tbody>
-                          <?php 
+                      <tbody class="text-center">
+                          <?php
+
                           $sql_query = "SELECT * FROM resultados ";//join usuario on usuario.email
                           $resultset = mysqli_query($con, $sql_query) or die("error base de datos:". mysqli_error($con));
                           while( $libro = mysqli_fetch_assoc($resultset) ) {
+                         
                           ?>
-                             <tr id="<?php echo $libro ['id']; ?>">
+                             <tr id="<?php echo $libro ['id_usuario']; ?>">
                              <td><?php echo $libro ['id']; ?></td>
                              <td><?php echo $libro ['nombre']; ?></td>
                              <td><?php echo $libro ['apellido']; ?></td>
                              <td><?php echo $libro ['nivel de prueba']; ?></td>
-                             <td><a class="btn btn-primary fa fa-download" href="../../controlador/pdf.php"></a></td>   
-                             <td><a class="btn btn-primary fa fa-eye" href="../../controlador/mpdf/pdf/index.php"></a></td>   
-                             <td><a class="btn btn-primary fa fa-share-square" href="../../controlador/phpmailer/enviarC.php?CorreoA='<?php echo $libro ['email']; ?>'"></a></td>   
+                             <td><a class="btn btn-primary fa fa-download" href="../../controlador/pdf.php">Descarga de pdf</a></td>   
+                             <td><a class="btn btn-primary fa fa-eye" href="../../controlador/mpdf/pdf/index.php">Ver el pdf</a></td>   
+                             <td><a class="btn btn-success fa fa-share-square" href="../../controlador/phpmailer/enviarC.php?CorreoA='<?php echo $libro ['id_usuario']; ?>'">Enviar correo contratado</a></td>   
+                             <td><a class="btn btn-danger fa fa-share-square" href="../../controlador/phpmailer/enviarCNO.php?CorreoA='<?php echo $libro ['id_usuario']; ?>'">Enviar correo no contratado</a></td>   
                             </tr>
                           <?php } ?>
+                          <?php  ?>
                       </tbody>
                   </table>    
             </div>
