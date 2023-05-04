1ec927683e9bdb42de06c01c1a5683f5f1a570af
alfonso300402 <rooseveltrodriguez3004@gmail.com>
2023-05-03 22:31:24 -0500

    ya casi esta el pdf

--- controlador/mpdf/pdf/plantilla/index.php
+++ controlador/mpdf/pdf/plantilla/index.php
@@ -1,84 +1,76 @@
-<?php 
-  function getplantilla(){
+<?php
+
+
+
+
+function getplantilla(){
+  
+  include("../../../configuracion/controller/conexion.php");
+  $conexion = new Conexion();
+  $con = $conexion->conectarDB();
+  
+  $_GET['id'];
+  $usuario = $_GET['id'];
+  
+  $sql = "SELECT * FROM usuario where idUsuario = $usuario";
+  $resultset = mysqli_query($con, $sql) or die("error base de datos:". mysqli_error($con));
+  $dato=mysqli_fetch_assoc($resultset);
+   
+  $nombre = $dato ['nombre'];
 
   $plantilla = '<body>
   <header class="clearfix">
   <div id="logo">
-  <img src="./img/img1.png">
+  <img src="../../../img/empresa/NielRoo_logo.png">
   </div>
-    <h1>INVOICE 3-2-1</h1>
+    <h1>Resultado de la prueba del usuario '.$nombre.'</h1>
     <div id="company" class="clearfix">
-    <div>Company Name</div>
-    <div>455 Foggy Heights,<br /> AZ 85004, US</div>
-      <div>(602) 519-0450</div>
-      <div><a href="mailto:company@example.com">company@example.com</a></div>
+    <div>NielRoo</div>
+    <div>calle 26 #15-24 Tunja</div>
+      <div>316-2657240 - 302-2196510</div>
+      <div><a href="mailto:">proyectoGTH2022@gmail.com</a></div>
     </div>
     <div id="project">
-    <div><span>PROJECT</span> Website development</div>
-    <div><span>CLIENT</span> John Doe</div>
-    <div><span>ADDRESS</span> 796 Silver Harbour, TX 79273, US</div>
-    <div><span>EMAIL</span> <a href="mailto:john@example.com">john@example.com</a></div>
-    <div><span>DATE</span> August 17, 2015</div>
-    <div><span>DUE DATE</span> September 17, 2015</div>
+    <div><span>Proyecto</span> Pagina de talento humano</div>
+    <div><span>Nombre</span> '.$nombre.'</div>
+    <div><span>Apellido</span> Mayo 03, 2023</div>
+    <div><span>Fecha</span> Mayo 03, 2023</div>
     </div>
     </header>
     <main>
     <table>
       <thead>
         <tr>
-          <th class="service">SERVICE</th>
-          <th class="desc">DESCRIPTION</th>
-          <th>PRICE</th>
-          <th>QTY</th>
-          <th>TOTAL</th>
+          <th class="service">Pregunta</th>
+          <th class="desc">Respuesta</th>
           </tr>
           </thead>
           <tbody>
           <tr>
           <td class="service">Design</td>
           <td class="desc">Creating a recognizable design solution based on the companys existing visual identity</td>
-          <td class="unit">$40.00</td>
-          <td class="qty">26</td>
-          <td class="total">$1,040.00</td>
+
           </tr>
           <tr>
           <td class="service">Development</td>
           <td class="desc">Developing a Content Management System-based Website</td>
-          <td class="unit">$40.00</td>
-          <td class="qty">80</td>
-          <td class="total">$3,200.00</td>
+
           </tr>
           <tr>
           <td class="service">SEO</td>
           <td class="desc">Optimize the site for search engines (SEO)</td>
-          <td class="unit">$40.00</td>
-          <td class="qty">20</td>
-          <td class="total">$800.00</td>
+
           </tr>
           <tr>
           <td class="service">Training</td>
           <td class="desc">Initial training sessions for staff responsible for uploading web content</td>
-          <td class="unit">$40.00</td>
-          <td class="qty">4</td>
-          <td class="total">$160.00</td>
+
         </tr>
-        <tr>
-          <td colspan="4">SUBTOTAL</td>
-          <td class="total">$5,200.00</td>
-          </tr>
-          <tr>
-          <td colspan="4">TAX 25%</td>
-          <td class="total">$1,300.00</td>
-          </tr>
-          <tr>
-          <td colspan="4" class="grand total">GRAND TOTAL</td>
-          <td class="grand total">$6,500.00</td>
-          </tr>
           </tbody>
           </table>
           <div id="notices">
           <div>NOTICE:</div>
-          <div class="notice">A finance charge of 1.5% will be made on unpaid balances after 30 days.</div>
+          <div class="notice">Estos son resultados dado por un experto psicologo empleando sus conocimientos en el departamento de talento humano</div>
           </div>
           </main>
           <footer>
